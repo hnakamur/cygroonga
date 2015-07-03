4b54154504d1ba655b7292b6d8fa170615fec912
Hiroaki Nakamura <hnakamur@gmail.com>
2015-07-04 08:23:56 +0900

    (WIP) Try to specify error point
    python setup.py build_ext --inplace
    Compiling cygroonga.pyx because it changed.
    Cythonizing cygroonga.pyx
    
    Error compiling Cython file:
    ------------------------------------------------------------
    ...
            self._c_ctx = ccygroonga.grn_ctx_open(0)
    
        def db_create(self, path):
            c_path = path.encode('UTF-8')
            c_db = ccygroonga.grn_db_create(self._c_ctx, c_path, NULL)
            self._c_db = c_db
                            ^
    ------------------------------------------------------------
    
    cygroonga.pyx:24:25: Cannot convert 'grn_obj *' to Python object
    Traceback (most recent call last):
      File "setup.py", line 8, in <module>
        libraries=["groonga"])
      File
    "/Users/hnakamur/.anyenv/envs/pyenv/versions/3.4.3/lib/python3.4/site-packages/Cython/Build/Dependencies.py",
    line 865, in cythonize
        cythonize_one(*args[1:])
      File
    "/Users/hnakamur/.anyenv/envs/pyenv/versions/3.4.3/lib/python3.4/site-packages/Cython/Build/Dependencies.py",
    line 985, in cythonize_one
        raise CompileError(None, pyx_file)
    Cython.Compiler.Errors.CompileError: cygroonga.pyx

--- cygroonga.pyx
+++ cygroonga.pyx
@@ -3,13 +3,13 @@ cimport ccygroonga
 def init():
     return ccygroonga.grn_init()
 
-cdef class Database:
-    cdef ccygroonga.grn_ctx* _c_ctx
-    cdef ccygroonga.grn_obj* _c_cb
-
-    def __cinit__(self, ccygroonga.grn_ctx* c_ctx, ccygroonga.grn_obj* c_db):
-        self._c_ctx = c_ctx
-        self._c_db = c_db
+#cdef class Database:
+#    cdef ccygroonga.grn_ctx* _c_ctx
+#    cdef ccygroonga.grn_obj* _c_cb
+#
+#    def __cinit__(self, ccygroonga.grn_ctx* c_ctx, ccygroonga.grn_obj* c_db):
+#        self._c_ctx = c_ctx
+#        self._c_db = c_db
 
 cdef class Context:
     cdef ccygroonga.grn_ctx* _c_ctx
@@ -21,7 +21,8 @@ cdef class Context:
     def db_create(self, path):
         c_path = path.encode('UTF-8')
         c_db = ccygroonga.grn_db_create(self._c_ctx, c_path, NULL)
-        return Database(self._c_ctx, c_db)
+        self._c_db = c_db
+        #return Database(self._c_ctx, c_db)
 
     def fin(self):
         return ccygroonga.grn_ctx_fin(self._c_ctx)

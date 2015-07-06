9f53f9ddf3b52ad2445c92fc4327d08681817240
Hiroaki Nakamura <hnakamur@gmail.com>
2015-07-06 13:45:53 +0900

    Add test for create_index_column

--- tests/test_table.py
+++ tests/test_table.py
@@ -108,6 +108,30 @@ def test_add_record():
     finally:
         shutil.rmtree(work_dir)
 
+def test_get_record():
+    work_dir = tempfile.mkdtemp()
+    try:
+        with grn.Groonga():
+            with grn.Context() as ctx:
+                db_path = os.path.join(work_dir, "test.db")
+                ctx.create_database(db_path)
+                table1 = ctx.create_table("Table1",
+                        grn.OBJ_TABLE_HASH_KEY | grn.OBJ_PERSISTENT,
+                        ctx.at(grn.DB_SHORT_TEXT))
+
+                table1.create_column("content",
+                        grn.OBJ_PERSISTENT | grn.OBJ_COLUMN_SCALAR,
+                        ctx.at(grn.DB_TEXT))
+                table1.create_column("updated_at",
+                        grn.OBJ_PERSISTENT | grn.OBJ_COLUMN_SCALAR,
+                        ctx.at(grn.DB_TIME))
+
+                record_id, added = table1.add_record("key1")
+                assert added
+                assert table1.get_record("key1"), record_id
+    finally:
+        shutil.rmtree(work_dir)
+
 def test_set_string():
     work_dir = tempfile.mkdtemp()
     try:
@@ -149,5 +173,20 @@ def test_set_time():
                 time1 = datetime.datetime(2015, 5, 24, 12, 34, 56, 789)
                 column.set_time(record_id, time1)
                 assert column.get_time(record_id), time1
+    finally:
+        shutil.rmtree(work_dir)
+
+def test_set_default_tokenizer():
+    work_dir = tempfile.mkdtemp()
+    try:
+        with grn.Groonga():
+            with grn.Context() as ctx:
+                db_path = os.path.join(work_dir, "test.db")
+                ctx.create_database(db_path)
+                table1_index = ctx.create_table("Table1Index",
+                        grn.OBJ_TABLE_PAT_KEY | grn.OBJ_KEY_NORMALIZE |
+                        grn.OBJ_PERSISTENT,
+                        ctx.at(grn.DB_SHORT_TEXT))
+                table1_index.set_default_tokenizer("TokenBigram")
     finally:
         shutil.rmtree(work_dir)
